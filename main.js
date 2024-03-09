99d26f160bbfc4b63d2de67c7a61851c6be50b65
Aniekan Essiet <essiet.aniekan@yahoo.com>
2024-03-09 14:46:38 +0300

    wrote the js

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-const text = "Hello, world!";
+function calc() {
+    //Get the HTML element
+    //Using the Value attribute get the value
+    //Use the Number attribute to change the type from string to number
+    
+    const num = document.getElementById("num1")
+    const numValue = num.value
+    const fig = Number(numValue)
 
-console.log(text);
+    const num1 = document.getElementById("num2")
+    const numValue1 = num1.value
+    const fig1 = Number(numValue1)
+
+    //Formula for calculating percents
+    //Final Value - Initial Value
+    //Dividide the difference by the initial value
+    //Then multiply by hundred
+
+    const difference = fig1 - fig
+    const divided = difference / fig
+    const finalAnswer = divided * 100
+
+    document.getElementById("answer").innerHTML = finalAnswer + "%";
+}
